408e22f3a203beb140c557cdf1e7a1a915dc3b7d
edbarz9 <charles@tinson.cc>
2019-02-18 10:12:25 +0100

    few tweaks with sawtooth

--- main.go
+++ main.go
@@ -26,15 +26,24 @@ func playfreq(s *pulse.Stream, ss *pulse.SampleSpec, f int) {
   tau := 2 * math.Pi
   data := make([]byte, 8*ss.Rate)
   r := float64(ss.Rate)
-  for i := 0; i < int(ss.Rate); i++ {
+  for i := 0; i < 2*int(ss.Rate); i++ {
     sample := float32((math.Sin(tau*float64(f*i)/r) / 3.0)  * float64(i)/(r))
     bits := math.Float32bits(sample)
 		binary.LittleEndian.PutUint32(data[4*i:4*i+4], bits)
   }
   s.Write(data)
-  for i := 0; i < int(ss.Rate); i++ {
+  for i := 0; i < 2 * int(ss.Rate); i++ {
     //sawtooth wav
-    sample := float32(i % (44100/50) / 2)
+    sample := float32(i % (44100/150)) * 0.0001
+    //sample := float32(math.Sin(tau*float64(f*i)/r))
+    bits := math.Float32bits(sample)
+		binary.LittleEndian.PutUint32(data[4*i:4*i+4], bits)
+  }
+  s.Write(data)
+  s.Write(data)
+  for i := 0; i < 2 * int(ss.Rate); i++ {
+    //sawtooth wav
+    sample := float32(i % (44100/180)) * 0.0001
     //sample := float32(math.Sin(tau*float64(f*i)/r))
     bits := math.Float32bits(sample)
 		binary.LittleEndian.PutUint32(data[4*i:4*i+4], bits)
