4e89fc1b76764c15e34045ada2d02ea03809c96c
edbarz9 <charles@tinson.cc>
2019-02-21 11:05:39 +0100

    sawtooth actually fix

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"math"
   "strconv"
   "os"
+  "gonum.org/v1/plot"
+  "gonum.org/v1/plot/plotter"
 )
 
 const sample_rate uint32 = 44100;
@@ -28,13 +30,17 @@ func main() {
 
 func playfreq(s *pulse.Stream, ss *pulse.SampleSpec, f uint32) {
   data := make([]byte, 2*44100)
+  plot := make([]uint16, 44100)
   var i uint32;
   for i = 0; i < 44100; i++ {
     //bits := sinewave(f, i)
     bits := sawtooth(f, i)
+    //bits := triangle(f, i)
+    plot[i] = bits
 		binary.LittleEndian.PutUint16(data[2*i:2*i+2], bits)
   }
   s.Write(data)
+  int2plot(plot)
 }
 
 func sinewave (freq uint32, phase uint32) uint16{
@@ -43,9 +49,42 @@ func sinewave (freq uint32, phase uint32) uint16{
 }
 
 func sawtooth (freq uint32, phase uint32) uint16{
-  //TODO fix this
-  //p := float32(phase)
-  srf := sample_rate/freq
-  sample := (float32(phase % srf)/float32(srf))-1;
+  var sample float32 = 0;
+  spo := sample_rate/freq
+  sample = (float32(phase % spo)/float32(spo)) * 2 -1;
   return uint16(sample * 32000);
+}
+
+func triangle (freq uint32, phase uint32) uint16{
+  var sample float32 = 0;
+  spo := sample_rate/freq;
+  spo2 := spo / 2;
+  pspo := float32(phase % spo);
+  if (phase % spo) < spo2 {
+    sample = pspo / float32(spo) - 1;
+  } else {
+    sample = 1 - pspo / float32(spo);
+  }
+  return uint16(sample * 32000)
+}
+
+func int2XYs(intbuff []uint16) (plotter.XYs) {
+  var xys plotter.XYs
+  for i:=0;i<1000;i++{
+    xys = append(xys, struct{ X, Y float64 }{float64(i), float64(intbuff[i])})
+  }
+  return xys
+}
+
+func int2plot(intbuff []uint16) {
+  path := "plot.png"
+  f, _ := os.Create(path)
+  p, _ := plot.New()
+  xys := int2XYs(intbuff)
+  line, _ := plotter.NewLine(xys)
+  line.StepStyle = plotter.PostStep
+  p.Add(line)
+  wt, _ := p.WriterTo(2000,1500,"png")
+  wt.WriteTo(f)
+  f.Close()
 }
